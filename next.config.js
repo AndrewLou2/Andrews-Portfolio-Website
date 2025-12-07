df77f94d8d1e6860ca468b45a0464218aff0a53b
Andrew Lou <alou2@uwo.ca>
2025-12-06 22:54:10 -0500

    build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // For GitHub Pages deployment
+  basePath: "/Andrews-Portfolio-Website",
+  assetPrefix: "/Andrews-Portfolio-Website/",
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
